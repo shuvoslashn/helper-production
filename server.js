b4b1977fc10614c64b694d8a815617a5269e388f
Shuvo Sarker <34802984+shuvoslashn@users.noreply.github.com>
2023-09-17 08:07:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
-import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
